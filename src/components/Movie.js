fb3c33e521ee1e447180a835fcac823b2e01b397
aungmcs <amhtet.cs@gmail.com>
2020-11-02 08:46:30 +0630

    changed movie card hover effect

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,23 +4,34 @@ import moviePlaceholder from "../img/moviePic.png";
 
 const Movie = ({ movie, handleMovieInfo }) => {
   return (
-    <div className="movie-box relative w-full">
+    <Link
+      className="movie-box relative w-full cursor-pointer"
+      to={`movie/${movie.imdbID}`}
+      onClick={() => handleMovieInfo(movie.imdbID)}
+    >
       <img
         className="movie-img rounded-xl w-full h-full object-center mx-auto shadow-lg"
         src={movie.Poster === "N/A" ? moviePlaceholder : movie.Poster}
         alt="movie poster"
       />
 
-      <div className="movie-button absolute bottom-0 rounded-xl opacity-0 flex flex-col items-center justify-end h-full w-full">
-        <Link
-          to={`movie/${movie.imdbID}`}
-          className="bg-main w-3/4 cursor-pointer text-white text-center py-2 px-3 text-xs sm:text-sm rounded mb-3"
-          onClick={() => handleMovieInfo(movie.imdbID)}
+      <div className="movie-button absolute bottom-0 rounded-xl opacity-0 flex flex-col items-center justify-center h-full w-full">
+        <svg
+          className="bg-main text-white rounded-full"
+          width="40"
+          height="40"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
-          More Info
-        </Link>
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clipRule="evenodd"
+          />
+        </svg>
       </div>
-    </div>
+    </Link>
   );
 };
 
